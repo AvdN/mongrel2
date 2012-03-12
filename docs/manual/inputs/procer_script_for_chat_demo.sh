f2783ce8f76916e180684dc19c2b7ff84c940d54
Anthon van der Neut <anthon@mnt.org>
2012-03-12 12:44:48 +0100

    added subsection on use and setting of environment vars (PROCER_RUN_LOG, PROCER_ERROR_LOG PROCER_ACTION, PROCER_PIDFILE). Updated chat/run to use PROCER_PIDFILE.

--- docs/manual/inputs/procer_script_for_chat_demo.sh
+++ docs/manual/inputs/procer_script_for_chat_demo.sh
@@ -3,9 +3,12 @@ set -e
 
 DEPLOY=/home/YOU/deployment
 SOURCE=/home/YOU/projects/mongrel2
+if test $PROCER_PIDFILE"x" == "x" ; then
+  PROCER_PIDFILE=$DEPLOY/profiles/chat/chat.pid
+fi
 
 cd $SOURCE/examples/chat
 # WARNING: on some systems the nohup doesn't work, like OSX
 # try running it without
 nohup python -u chat.py > chat.log 2>&1 &
-echo $! > $DEPLOY/profiles/chat/chat.pid
+echo $! > $PROCER_PIDFILE
